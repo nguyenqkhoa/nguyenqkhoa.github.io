2067fe3efd54cc2c2bb1267ec8f61a45e376c8c3
nguyenqkhoa <nguyenqkhoa@hotmail.com>
2024-01-16 03:41:27 -0700

    Added multiple images to each view
    Added multiple images to each view, as well as some more Cupa artists

--- pages/cupaIndex2Script.js
+++ pages/cupaIndex2Script.js
@@ -1,3 +1,4 @@
+/*
 function openLightbox(imageSrc, textContent) {
     var lightbox = document.getElementById('lightboxContainer');
     var lightboxImage = document.querySelector('.lightbox-image');
@@ -5,7 +6,34 @@ function openLightbox(imageSrc, textContent) {
 
     lightbox.style.display = 'flex';
     lightbox.classList.add('blurred-background');
+
+    lightboxImage.innerHTML = '';
+
     lightboxImage.src = imageSrc;
+    textContainer.innerHTML = textContent;
+}
+*/
+
+function openLightbox(images, textContent) {
+    var lightbox = document.getElementById('lightboxContainer');
+    var lightboxImage = document.querySelector('.lightbox-image');
+    var textContainer = document.getElementById('textContainer');
+
+    lightbox.style.display = 'flex';
+    lightbox.classList.add('blurred-background');
+    
+    var imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
+    images.forEach(function (imageSrc){
+        var img = document.createElement('img');
+        img.src = imageSrc;
+        imageContainer.appendChild(img);
+    });
+
+    lightboxImage.innerHTML = '';
+    lightboxImage.appendChild(imageContainer);
+
     textContainer.innerHTML = textContent;
 }
 
@@ -13,6 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var lightbox = document.getElementById('lightboxContainer');
     var lightboxImage = document.querySelector('.lightbox-image');
     var textContainer = document.getElementById('textContainer');
+    var xButton = document.getElementById('xButton');
 
     lightbox.addEventListener('click', function (event) {
         if (event.target === lightbox) {
@@ -21,6 +50,12 @@ document.addEventListener('DOMContentLoaded', function () {
             textContainer.innerHTML = '';
         }
     });
+
+    xButton.addEventListener('click', function(){
+        lightbox.style.display = 'none';
+        lightbox.classList.remove('blurred-background');
+        textContainer.innerHTML = '';
+    })
 
     lightboxImage.addEventListener('click', function (event) {
         event.stopPropagation();
